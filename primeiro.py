d53407d62ee0067a41d66997f4daecd3d9b02b1f
Daniele Freitas <71996747+Danny-ctrl@users.noreply.github.com>
2023-04-16 14:18:04 -0300

    first

---
+++ primeiro.py
@@ -0,0 +1,6 @@
+# primeira aula
+print('Hello World!')
+print('seja bem vindo ao curso')
+
+linguagem = input('Qual linguagem de programação voce esta estudando?')
+print('a linguagem que vocêestá estudando é ', linguagem)
---
+++ variaveis.py
@@ -0,0 +1,18 @@
+
+# segunda aula
+idade = 26  # criando uma variável int
+print(idade)
+
+nome = 'Daniele Freitas'  # criando variável str
+print(nome)
+
+altura = 1.77  # criando variável float
+print(altura)
+
+estudando = True  # criando variável boll
+print(estudando)
+
+print(type(idade))
+print(type(nome))
+print(type(altura))
+print(type(estudando))
