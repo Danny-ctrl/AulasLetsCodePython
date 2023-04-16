70672f7f2f8d8a1e86976ea3423f9c00dbfd92be
Daniele Freitas <71996747+Danny-ctrl@users.noreply.github.com>
2023-04-16 19:55:56 -0300

    conditional structures

---
+++ condicionais.py
@@ -0,0 +1,29 @@
+# Estruturas condicionais
+
+"""idade = 18
+
+if idade >= 18:
+    print('Você é maior de idade')
+
+else:
+    print('Você é menor de idade')
+
+media = float(input('Informe a média do estudante: '))
+
+if media >= 7:
+    print('Você foi aprovado(a)!')
+
+elif media >= 5:
+    print('Você está de recuperação!')
+
+
+else:
+    print('Você foi reprovado(a)!')"""
+
+media = 10
+presenca = 100
+
+if media >= 7 and presenca >= 70:
+    print('Aprovado(a)!')
+else:
+    print('Reprovado')
